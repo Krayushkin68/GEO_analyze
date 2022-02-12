09d9b2261b2fe6c36f00e1bf5885e698df756fac
unknown <krayuskhinml97@gmail.com>
2022-02-12 15:56:55 +0300

    update examples

--- example.py
+++ example.py
@@ -4,6 +4,7 @@ from pygeoanalyze import Infrastructure
 
 TOKEN = json.load(open('token.json', 'rt')).get('TOKEN')
 
+# EXAMPLE 1
 inf_osm = Infrastructure(address='Люберцы, Барыкина 8', search_range=800, source=Infrastructure.sources.OSM)
 if inf_osm.analyze():
     print(inf_osm.received_info)
@@ -11,6 +12,7 @@ if inf_osm.analyze():
     inf_osm.save_to_json('data/map_osm.json')
     inf_osm.draw_map('data/map_osm.png')
 
+# EXAMPLE 2
 inf_ya = Infrastructure(address='Люберцы, Барыкина 8', search_range=500, source=Infrastructure.sources.Yandex,
                         token=TOKEN)
 if inf_ya.analyze():
@@ -18,13 +20,14 @@ if inf_ya.analyze():
     inf_ya.save_to_json('data/map_ya.json')
     inf_ya.draw_map('data/map_ya.png')
 
+# EXAMPLE 3
 interested_addresses = [
     'Москва, ул. Юных Ленинцев 38',
     'Москва, 2-й Карачаровский проезд, 4',
     'Москва, Нижегородская улица, 13А',
     'Москва, Рабочая улица, 6к1'
 ]
-inf = Infrastructure(source='Yandex', token=TOKEN, search_range=500)
+inf = Infrastructure(source=Infrastructure.sources.Yandex, token=TOKEN, search_range=500)
 data = dict().fromkeys(interested_addresses)
 for addr in interested_addresses:
     inf.set_address(addr)
